5c63bf7112f3bec6a2f14cd3645f265667e9726e
Tim Felgentreff <timfelgentreff@gmail.com>
2010-12-22 00:54:15 +0100

    FFI Wrapper to run bcrypt-ruby on Maglev * compiles to shared object using a custom Makefile * loads bcrypt_engine which wraps the shared object using FFI to   offer the same interface as the plain C extension

--- ext/mri/extconf.rb
+++ ext/mri/extconf.rb
@@ -9,6 +9,29 @@ if RUBY_PLATFORM == "java"
     f.puts "\t@true"
   end
   exit 0
+elsif RUBY_ENGINE == "maglev"
+  # Maglev doesn't support C extensions, fall back to compiling an FFI usable
+  # library
+  File.open('Makefile', 'w') do |f|
+    f.puts <<-MAKEFILE
+CFLAGS = -fPIC
+OBJS = bcrypt.o blowfish.o
+DLIB = bcrypt_ext.so
+OS ?= $(strip $(shell uname -s | tr '[:upper:]' '[:lower:]'))
+ifeq ($(OS),darwin)
+	DLIB = bcrypt_ext.dylib
+	CFLAGS += -dynamiclib
+endif
+
+all: $(OBJS)
+	cc -shared -o $(DLIB) $(OBJS)
+install:
+	install $(DLIB) "../../lib/"
+clean:
+	$(RM) $(OBJS) bcrypt_ext.so
+    MAKEFILE
+  end
+  exit 0
 else
   require "mkmf"
   dir_config("bcrypt_ext")
