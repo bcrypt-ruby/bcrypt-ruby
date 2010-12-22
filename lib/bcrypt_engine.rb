1450a61dd2c0699c68e93d7e44a6e8652808d6d8
Tim Felgentreff <timfelgentreff@gmail.com>
2010-12-22 11:32:16 +0100

    FFI wrapper

---
+++ lib/bcrypt_engine.rb
@@ -0,0 +1,38 @@
+require 'ffi'
+
+module BCrypt
+  class Engine
+    class Uint8Ptr < FFI::Struct
+      layout :value, :uint8
+    end
+
+    extend FFI::Library
+
+    BCRYPT_MAXSALT = 16
+    BCRYPT_SALT_OUTPUT_SIZE = 7 + (BCRYPT_MAXSALT * 4 + 2) / 3 + 1
+    BCRYPT_OUTPUT_SIZE = 128
+
+    ffi_lib File.expand_path("../bcrypt_ext.so", __FILE__)
+
+    attach_function :ruby_bcrypt, [:buffer_out, :string, :string], :string
+    attach_function :ruby_bcrypt_gensalt, [:buffer_out, :uint8, :pointer], :string
+
+    def self.__bc_salt(cost, seed)
+      buffer_out = FFI::Buffer.alloc_out(BCRYPT_SALT_OUTPUT_SIZE, 1)
+      seed_ptr = FFI::MemoryPointer.new(:uint8, 1)
+      seed_ptr.int8_put(0, seed % 255)
+      out = ruby_bcrypt_gensalt(buffer_out, cost, seed_ptr)
+      seed_ptr.free
+      buffer_out.free
+      out || ""
+    end
+
+    def self.__bc_crypt(key, salt, cost)
+      buffer_out = FFI::Buffer.alloc_out(BCRYPT_OUTPUT_SIZE, 1)
+      out = ruby_bcrypt(buffer_out, key || "", salt)
+      buffer_out.free
+      out && out.any? ? out : nil
+    end
+  end
+end
+
