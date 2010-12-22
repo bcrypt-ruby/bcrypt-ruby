2ea8af31823b46db5c0c10723783e44c62a04b1b
Tim Felgentreff <timfelgentreff@gmail.com>
2010-12-22 09:05:32 +0100

    compile only the shared object on maglev

--- ext/mri/extconf.rb
+++ ext/mri/extconf.rb
@@ -9,6 +9,27 @@ if RUBY_PLATFORM == "java"
     f.puts "\t@true"
   end
   exit 0
+elsif RUBY_ENGINE == "maglev"
+  # Maglev doesn't support C extensions, fall back to compiling an FFI usable
+  # library
+  File.open('Makefile', 'w') do |f|
+    f.puts <<-MAKEFILE
+CFLAGS = -fPIC
+OBJS = bcrypt.o blowfish.o
+DLIB = bcrypt.so
+OS ?= $(strip $(shell uname -s | tr '[:upper:]' '[:lower:]'))
+ifeq ($(OS),darwin)
+	DLIB = bcrypt.bundle
+endif
+
+all: $(OBJS)
+	cc -shared -o $(DLIB) $(OBJS)
+
+clean:
+	$(RM) $(OBJS) bcrypt.so
+    MAKEFILE
+  end
+  exit 0
 else
   require "mkmf"
   dir_config("bcrypt_ext")
