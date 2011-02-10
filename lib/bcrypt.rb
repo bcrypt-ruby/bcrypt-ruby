697b93db79cf10dcb480e9260337517ae93d75dc
Aaron Patterson <aaron.patterson@gmail.com>
2011-02-10 13:04:13 -0800

    fix unused variable complaints in 1.9.3

--- lib/bcrypt.rb
+++ lib/bcrypt.rb
@@ -187,7 +187,7 @@ module BCrypt
     #
     # Splits +h+ into version, cost, salt, and hash and returns them in that order.
     def split_hash(h)
-      b, v, c, mash = h.split('$')
+      _, v, c, mash = h.split('$')
       return v, c.to_i, h[0, 29].to_str, mash[-31, 31].to_str
     end
   end
