0b6d5ff41669a1e489ab37f1eb05641d660038a0
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2018-04-14 10:12:29 +1000

    Better be careful than sorry

--- ext/mri/ow-crypt.h
+++ ext/mri/ow-crypt.h
@@ -24,7 +24,7 @@
 
 #ifndef __SKIP_GNU
 extern char *crypt(__const char *key, __const char *setting);
-#if (__FreeBSD_version >= 1200000)
+#if (defined(__FreeBSD__) && (__FreeBSD_version >= 1200000))
 extern char *crypt_r(__const char *key, __const char *setting, struct crypt_data *data);
 #else
 extern char *crypt_r(__const char *key, __const char *setting, void *data);
